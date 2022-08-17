44df73e88c07478d5d9565c746736319bc501256
Ana Muraya <Anamuraya9@gmail.com>
2022-08-17 12:06:27 +0300

    Created the models and db connection

--- server.js
+++ server.js
@@ -10,6 +10,41 @@ app.use(cors(corsOptions));
 app.use(express.json()); // parse requests of content-type - application/json
 app.use(express.urlencoded({ extended: true })); // parse requests of content-type - application/x-www-form-urlencoded
 
+const db = require("./app/models");
+const dbConfig = require("./app/config/db.config");
+const Role = db.role;
+
+//open mongoose connection to MongoDB
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`Successfully connected to MongoDB`);
+    initial();
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+//creating the two rows in roles collection
+const initial = () => {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({ name: "user" }).save((err) => {
+        err && console.log("error", err);
+        console.log(`Added user to roles collection`);
+      });
+      new Role({ name: "admin" }).save((err) => {
+        err && console.log("error", err);
+        console.log(`Added admin to roles collection`);
+      });
+    }
+  });
+};
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to fitness application" });
 });
