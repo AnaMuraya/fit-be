fdea3a3fe32f3bf8e4f08f0e31b7ab8f94d0e013
Ana Muraya <Anamuraya9@gmail.com>
2022-09-05 18:35:10 +0300

    ...

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -4,7 +4,7 @@ module.exports = userRoutes = (app) => {
   app.use((req, res, next) => {
     res.header(
       "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
+      "x-access-token, Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   });
